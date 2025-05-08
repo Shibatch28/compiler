6e943c074bd0733a6cd5330455640635cc78c2e4
Shibatch <mail@48inunokoya.net>
2025-05-08 20:00:51 +0900

    style: キモいインデント抜けを修正

--- 9cc.c
+++ 9cc.c
@@ -5,7 +5,7 @@ int main(int argc, char **argv) {
   if (argc != 2) {
     fprintf(stderr, "引数の個数が不正です．\n");
     return 1;
-}
+  }
 
   printf(".intel_syntax noprefix\n");
   printf(".globl main\n");
